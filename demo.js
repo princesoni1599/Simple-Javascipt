c3d34a257f8bde6374b0f07fc960ab3b9a280dd2
princesoni1599 <54708454+princesoni1599@users.noreply.github.com>
2021-10-22 09:19:47 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,15 @@
+document.write("external file kallu");
+var x=3;
+document.write(x);
+if(x<0)
+{
+    document.write("<h3> negattive</h3>")
+}
+else if(x>0)
+{
+    document.write("<h3>positive</h3>")
+}
+else
+{
+    document.write("<h3>positive</h3>")
+}
